7e379a870e551f0eba266d8fcdf955c41490a8e2
Randy <randyho@outlook.com>
2023-11-01 16:20:10 +0800

    Update diy-part1_openwrt.sh

--- diy-part1_openwrt.sh
+++ diy-part1_openwrt.sh
@@ -18,9 +18,9 @@
 # Add luci-theme-argon
 # rm -rf lede/package/lean/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-theme-argon.git
-rm -rf package/lean/luci-theme-argon/
-git clone --depth 1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth 1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# rm -rf package/lean/luci-theme-argon/
+# git clone --depth 1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+# git clone --depth 1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 添加额外软件包
 git clone --depth 1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
