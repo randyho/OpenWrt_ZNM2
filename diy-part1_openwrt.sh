968d86e5ace204bfd278535b506f1fb98c787543
Randy <randyho@outlook.com>
2024-03-29 11:59:39 +0800

    Update diy-part1_openwrt.sh

--- diy-part1_openwrt.sh
+++ diy-part1_openwrt.sh
@@ -23,5 +23,4 @@
 # git clone --depth 1 -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 # 添加额外软件包
-git clone --depth 1 -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-# git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/randyho/luci-app-clash-lite.git package/luci-app-clash-lite
