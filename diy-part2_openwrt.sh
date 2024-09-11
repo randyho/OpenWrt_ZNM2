25e7bf72bc63fd076ece014f272729ee95821074
Randy <randyho@outlook.com>
2024-09-11 13:55:00 +0800

    Update diy-part2_openwrt.sh

--- diy-part2_openwrt.sh
+++ diy-part2_openwrt.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改 argon 为默认主题,不再集成luci-theme-bootstrap主题
-# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 添加网口
 # sed -i 's/eth0/eth0 eth2 eth3/g' package/base-files/files/etc/board.d/99-default_network
@@ -24,4 +24,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # sed -i '$i uci commit network' package/*/*/my-default-settings/files/uci.defaults
 
 # 添加默认密码
-sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
+#sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' package/base-files/files/etc/shadow
