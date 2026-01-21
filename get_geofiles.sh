b8b0ec48fc38da3f8b5182e87b7613affdfc0364
Randy <randyho@outlook.com>
2026-01-21 16:26:03 +0800

    Update get_geofiles.sh

--- get_geofiles.sh
+++ get_geofiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p files/etc/nikki
+mkdir -p files/etc/nikki/run
 
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-wget -qO- $GEOIP_URL > files/etc/nikki/GeoIP.dat
-wget -qO- $GEOSITE_URL > files/etc/nikki/GeoSite.dat
+wget -qO- $GEOIP_URL > files/etc/nikki/run/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/nikki/run/GeoSite.dat
