48d24368295c98a46e7dad1e0c8e930fbfb0d3ef
Randy <randyho@outlook.com>
2026-06-30 14:03:50 +0800

    Update get_geofiles.sh

--- get_geofiles.sh
+++ get_geofiles.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+set -e
 
 mkdir -p files/etc/nikki/run
 
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-MMDB_URL="https://github.com/Loyalsoldier/geoip@release/Country-without-asn.mmdb"
-ASN_URL="https://github.com/Loyalsoldier/geoip@release/GeoLite2-ASN.mmdb"
+MMDB_URL="https://github.com/Loyalsoldier/geoip/releases/latest/download/Country-without-asn.mmdb"
+ASN_URL="https://github.com/Loyalsoldier/geoip/releases/latest/download/GeoLite2-ASN.mmdb"
 
-wget -qO- $GEOIP_URL > files/etc/nikki/run/GeoIP.dat
-wget -qO- $GEOSITE_URL > files/etc/nikki/run/GeoSite.dat
-wget -qO- $MMDB_URL > files/etc/nikki/run/Country.mmdb
-wget -qO- $ASN_URL > files/etc/nikki/run/ASN.mmdb
+wget -O files/etc/nikki/run/GeoIP.dat "$GEOIP_URL"
+wget -O files/etc/nikki/run/GeoSite.dat "$GEOSITE_URL"
+wget -O files/etc/nikki/run/Country.mmdb "$MMDB_URL"
+wget -O files/etc/nikki/run/ASN.mmdb "$ASN_URL"
+
