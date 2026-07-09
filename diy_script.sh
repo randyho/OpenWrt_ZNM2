e87e80f7eac0df17e30b30b419cb793e2d6cc935
Randy <randyho@outlook.com>
2026-07-09 10:07:22 +0800

    Update diy_script.sh

--- diy_script.sh
+++ diy_script.sh
@@ -17,9 +17,6 @@ rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/applications/luci-app-serverchan
 rm -rf feeds/luci/applications/luci-app-smartdns
 
-# 科学上网插件
-git clone --depth 1 -b main https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
-
 # 修改本地时间格式
 # sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' package/lean/autocore/files/*/index.htm
 
@@ -29,6 +26,22 @@ sed -i "s/^CONFIG_VERSION_CODE=.*/CONFIG_VERSION_CODE=\"R$(date +%Y%m%d)\"/" .co
 
 # 修改默认主题
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# 科学上网插件
+git clone --depth 1 -b main https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
+
+# 下载geo文件
+mkdir -p files/etc/nikki/run
+
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+MMDB_URL="https://raw.githubusercontent.com/Loyalsoldier/geoip/release/Country.mmdb"
+ASN_URL="https://raw.githubusercontent.com/Loyalsoldier/geoip/release/GeoLite2-ASN.mmdb"
+
+wget -O files/etc/nikki/run/GeoIP.dat "$GEOIP_URL"
+wget -O files/etc/nikki/run/GeoSite.dat "$GEOSITE_URL"
+wget -O files/etc/nikki/run/Country.mmdb "$MMDB_URL"
+wget -O files/etc/nikki/run/ASN.mmdb "$ASN_URL"
 
 # 再次更新feeds
 ./scripts/feeds update -a
