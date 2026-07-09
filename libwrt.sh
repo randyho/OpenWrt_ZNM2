a13b6836392918cec516e2662d4fce79da13a662
Randy <randyho@outlook.com>
2026-07-09 08:58:16 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -16,8 +16,5 @@ rm -rf feeds/luci/applications/luci-app-smartdns
 
 rm -rf package/emortal/luci-app-athena-led
 
-git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
-chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
-
 # 科学上网插件
 git clone --depth 1 -b main https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
